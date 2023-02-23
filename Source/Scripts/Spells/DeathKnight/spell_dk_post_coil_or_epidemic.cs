57f0620f0624e61d0f992e4ee9e599dc0498e62a
hater <hater.rip@proton.me>
2023-02-23 17:30:14 -0500

    rename sudden doom use file as shared between epidemic and coil

--- Source/Scripts/Spells/DeathKnight/spell_dk_post_coil_or_epidemic.cs
+++ Source/Scripts/Spells/DeathKnight/spell_dk_post_coil_or_epidemic.cs
@@ -10,34 +10,34 @@ using Game.Spells;
 
 namespace Scripts.Spells.DeathKnight;
 
-[SpellScript(47541)]
-[SpellScript(207317)]
-internal class spell_dk_post_coil_or_epidemic : SpellScript, IHasSpellEffects
+[SpellScript(47632)]
+[SpellScript(212739)]
+internal class spell_dk_post_coil_or_epidemic : SpellScript, ISpellAfterHit
 {
 	public List<ISpellEffect> SpellEffects { get; } = new();
 
 	public override bool Validate(SpellInfo spell)
 	{
-		return ValidateSpellInfo(DeathKnightSpells.DEATH_COIL, DeathKnightSpells.EPIDEMIC);
+		return ValidateSpellInfo(DeathKnightSpells.DEATH_COIL_DAMAGE, DeathKnightSpells.EPIDEMIC_DAMAGE);
 	}
 
-	public override void Register()
-	{
-		SpellEffects.Add(new EffectHandler(HandleDummy, 0, SpellEffectName.Dummy, SpellScriptHookType.EffectHitTarget));
-	}
-
-	private void HandleDummy(int effIndex)
+	public void AfterHit()
 	{
 		var caster = GetCaster();
 		if (caster != null) {
 			var target = GetHitUnit();
 			if (target != null) {
-				var suddenDoom = caster.GetAura(DeathKnightSpells.DEATH_COIL_SUDDEN_DOOM_AURA);
+				var deathRotApply = 1;
+                var suddenDoom = caster.GetAura(DeathKnightSpells.DEATH_COIL_SUDDEN_DOOM_AURA);
 				if (suddenDoom != null)
 				{
+					deathRotApply += 1;
 					suddenDoom.ModStackAmount(-1);
                 }
-			}
+
+                if (caster.HasAura(DeathKnightSpells.DEATH_ROT))
+                    caster.CastSpell(target, DeathKnightSpells.DEATH_ROT_AURA, new CastSpellExtraArgs(TriggerCastFlags.FullMask).AddSpellMod(SpellValueMod.AuraStack, deathRotApply));
+            }
 		}
 	}
 }
