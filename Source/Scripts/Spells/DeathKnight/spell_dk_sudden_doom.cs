14cb96df53538cfd4faf25458d1a0e4c75434dd8
hater <hater.rip@proton.me>
2023-02-23 13:42:04 -0500

    update sudden doom

---
+++ Source/Scripts/Spells/DeathKnight/spell_dk_sudden_doom.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Forged WoW LLC <https://github.com/ForgedWoW/ForgedCore>
+// Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/ForgedCore/blob/master/LICENSE> for full information.
+
+using System.Collections.Generic;
+using Framework.Constants;
+using Game.Scripting;
+using Game.Scripting.Interfaces;
+using Game.Scripting.Interfaces.ISpell;
+using Game.Spells;
+
+namespace Scripts.Spells.DeathKnight;
+
+[SpellScript(47541)]
+[SpellScript(207317)]
+internal class spell_dk_post_coil_or_epidemic : SpellScript, IHasSpellEffects
+{
+	public List<ISpellEffect> SpellEffects { get; } = new();
+
+	public override bool Validate(SpellInfo spell)
+	{
+		return ValidateSpellInfo(DeathKnightSpells.DEATH_COIL, DeathKnightSpells.EPIDEMIC);
+	}
+
+	public override void Register()
+	{
+		SpellEffects.Add(new EffectHandler(HandleDummy, 0, SpellEffectName.Dummy, SpellScriptHookType.EffectHitTarget));
+	}
+
+	private void HandleDummy(int effIndex)
+	{
+		var caster = GetCaster();
+		if (caster != null) {
+			var target = GetHitUnit();
+			if (target != null) {
+				var suddenDoom = caster.GetAura(DeathKnightSpells.DEATH_COIL_SUDDEN_DOOM_AURA);
+				if (suddenDoom != null)
+				{
+					suddenDoom.ModStackAmount(-1);
+                }
+			}
+		}
+	}
+}
