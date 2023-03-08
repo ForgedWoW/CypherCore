c9c5df513fa2120f747bc25e1a3eaed5c9119fc6
Jim Burlison <JBurlison@Gmail.com>
2023-03-08 06:58:41 -0500

    Fix tabs

--- Source/Game/Maps/GridNotifiers/UpdaterNotifier.cs
+++ Source/Game/Maps/GridNotifiers/UpdaterNotifier.cs
@@ -11,37 +11,37 @@ namespace Game.Maps;
 
 public class UpdaterNotifier : IGridNotifierWorldObject
 {
-	readonly uint _timeDiff;
-	LimitedThreadTaskManager _threadManager;
-	HashSet<WorldObject> _worldObjects = new HashSet<WorldObject>();
+    readonly uint _timeDiff;
+    LimitedThreadTaskManager _threadManager;
+    HashSet<WorldObject> _worldObjects = new HashSet<WorldObject>();
 
     public GridType GridType { get; set; }
 
     public UpdaterNotifier(uint diff, GridType gridType, LimitedThreadTaskManager taskManager)
-	{
-		_timeDiff = diff;
-		GridType   = gridType;
+    {
+        _timeDiff = diff;
+        GridType = gridType;
         _threadManager = taskManager;
 
     }
 
-	public void Visit(IList<WorldObject> objs)
-	{
-		for (var i = 0; i < objs.Count; ++i)
-		{
-			var obj = objs[i];
+    public void Visit(IList<WorldObject> objs)
+    {
+        for (var i = 0; i < objs.Count; ++i)
+        {
+            var obj = objs[i];
 
-			if (obj == null || obj.IsTypeId(TypeId.Player) || obj.IsTypeId(TypeId.Corpse))
-				continue;
+            if (obj == null || obj.IsTypeId(TypeId.Player) || obj.IsTypeId(TypeId.Corpse))
+                continue;
 
-			if (obj.IsInWorld)
+            if (obj.IsInWorld)
                 _worldObjects.Add(obj);
-		}
+        }
     }
 
-	public void ExecuteUpdate()
-	{
-		foreach (var obj in _worldObjects)
+    public void ExecuteUpdate()
+    {
+        foreach (var obj in _worldObjects)
             _threadManager.Schedule(() => obj.Update(_timeDiff));
     }
 }
