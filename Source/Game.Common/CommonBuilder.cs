2a2a15b7a62e636b0cfc5e67d9f435e9439aa90d
Jim Burlison <JBurlison@Gmail.com>
2023-03-23 16:28:30 -0400

    Added builder for common.

---
+++ Source/Game.Common/CommonBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Game.Common.Handlers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Game.Common
+{
+    public static class CommonBuilder
+    {
+        public static IServiceCollection AddCommon(this IServiceCollection services)
+        {
+            services.AddSingleton<AuthenticationHandler>();
+            services.AddSingleton<BattlenetHandler>();
+            services.AddSingleton<BattlepayHandler>();
+            services.AddSingleton<CharacterHandler>();
+            services.AddSingleton<ChatHandler>();
+            services.AddSingleton<GarrisonHandler>();
+            services.AddSingleton<GuildHandler>();
+            services.AddSingleton<HotfixHandler>();
+            services.AddSingleton<MythicPlusHandler>();
+            services.AddSingleton<QueryHandler>();
+            services.AddSingleton<SocialHandler>();
+            services.AddSingleton<TimeHandler>();
+            services.AddSingleton<TokenHandler>();
+            return services;
+        }
+    }
+}
