626346fa85c8162de3f6036233b2cc2b954e76b1
Jim Burlison <JBurlison@Gmail.com>
2023-05-07 16:25:29 -0400

    GarrisonHandler

--- Source/Forged.MapServer/OpCodeHandlers/GarrisonHandler.cs
+++ Source/Forged.MapServer/OpCodeHandlers/GarrisonHandler.cs
@@ -3,61 +3,58 @@
 
 using Forged.MapServer.Networking;
 using Forged.MapServer.Networking.Packets.Garrison;
+using Forged.MapServer.Server;
 using Framework.Constants;
 using Game.Common.Handlers;
+
+// ReSharper disable UnusedMember.Local
 
 namespace Forged.MapServer.OpCodeHandlers;
 
 public class GarrisonHandler : IWorldSessionHandler
 {
+    private readonly WorldSession _session;
+
+    public GarrisonHandler(WorldSession session)
+    {
+        _session = session;
+    }
+
     [WorldPacketHandler(ClientOpcodes.GarrisonCancelConstruction)]
     private void HandleGarrisonCancelConstruction(GarrisonCancelConstruction garrisonCancelConstruction)
     {
-        if (!_session.Player.GetNPCIfCanInteractWith(garrisonCancelConstruction.NpcGUID, NPCFlags.None, NPCFlags2.GarrisonArchitect))
+        if (_session.Player.GetNPCIfCanInteractWith(garrisonCancelConstruction.NpcGUID, NPCFlags.None, NPCFlags2.GarrisonArchitect) == null)
             return;
 
         var garrison = _session.Player.Garrison;
 
-        if (garrison != null)
-            garrison.CancelBuildingConstruction(garrisonCancelConstruction.PlotInstanceID);
+        garrison?.CancelBuildingConstruction(garrisonCancelConstruction.PlotInstanceID);
     }
 
     [WorldPacketHandler(ClientOpcodes.GarrisonGetMapData)]
     private void HandleGarrisonGetMapData(GarrisonGetMapData garrisonGetMapData)
     {
-        var garrison = _session.Player.Garrison;
-
-        if (garrison != null)
-            garrison.SendMapData(_session.Player);
+        _session.Player.Garrison?.SendMapData(_session.Player);
     }
 
     [WorldPacketHandler(ClientOpcodes.GarrisonPurchaseBuilding)]
     private void HandleGarrisonPurchaseBuilding(GarrisonPurchaseBuilding garrisonPurchaseBuilding)
     {
-        if (!_session.Player.GetNPCIfCanInteractWith(garrisonPurchaseBuilding.NpcGUID, NPCFlags.None, NPCFlags2.GarrisonArchitect))
+        if (_session.Player.GetNPCIfCanInteractWith(garrisonPurchaseBuilding.NpcGUID, NPCFlags.None, NPCFlags2.GarrisonArchitect) == null)
             return;
 
-        var garrison = _session.Player.Garrison;
-
-        if (garrison != null)
-            garrison.PlaceBuilding(garrisonPurchaseBuilding.PlotInstanceID, garrisonPurchaseBuilding.BuildingID);
+        _session.Player.Garrison?.PlaceBuilding(garrisonPurchaseBuilding.PlotInstanceID, garrisonPurchaseBuilding.BuildingID);
     }
 
     [WorldPacketHandler(ClientOpcodes.GarrisonRequestBlueprintAndSpecializationData)]
     private void HandleGarrisonRequestBlueprintAndSpecializationData(GarrisonRequestBlueprintAndSpecializationData garrisonRequestBlueprintAndSpecializationData)
     {
-        var garrison = _session.Player.Garrison;
-
-        if (garrison != null)
-            garrison.SendBlueprintAndSpecializationData();
+        _session.Player.Garrison?.SendBlueprintAndSpecializationData();
     }
 
     [WorldPacketHandler(ClientOpcodes.GetGarrisonInfo)]
     private void HandleGetGarrisonInfo(GetGarrisonInfo getGarrisonInfo)
     {
-        var garrison = _session.Player.Garrison;
-
-        if (garrison != null)
-            garrison.SendInfo();
+        _session.Player.Garrison?.SendInfo();
     }
 }
