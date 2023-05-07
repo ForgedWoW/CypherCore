11196da06525e9a18bb72465650cf17d5f4cf5c6
Jim Burlison <JBurlison@Gmail.com>
2023-05-07 13:26:49 -0400

    Convert BlackMarketHandlers

--- Source/Forged.MapServer/OpCodeHandlers/BlackMarketHandlers.cs
+++ Source/Forged.MapServer/OpCodeHandlers/BlackMarketHandlers.cs
@@ -2,73 +2,82 @@
 // Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/ForgedCore/blob/master/LICENSE> for full information.
 
 using Forged.MapServer.BlackMarket;
-using Forged.MapServer.Entities.Creatures;
 using Forged.MapServer.Entities.Items;
 using Forged.MapServer.Entities.Objects;
-using Forged.MapServer.Entities.Players;
 using Forged.MapServer.Networking;
 using Forged.MapServer.Networking.Packets.BlackMarket;
 using Forged.MapServer.Networking.Packets.Item;
 using Forged.MapServer.Networking.Packets.NPC;
+using Forged.MapServer.Server;
 using Framework.Constants;
 using Framework.Database;
 using Game.Common.Handlers;
 using Serilog;
 
+// ReSharper disable UnusedMember.Local
+
 namespace Forged.MapServer.OpCodeHandlers;
 
 public class BlackMarketHandlers : IWorldSessionHandler
 {
+    private readonly BlackMarketManager _blackMarketManager;
+    private readonly CharacterDatabase _characterDatabase;
+    private readonly WorldSession _session;
+
+    public BlackMarketHandlers(WorldSession session, BlackMarketManager blackMarketManager, CharacterDatabase characterDatabase)
+    {
+        _session = session;
+        _blackMarketManager = blackMarketManager;
+        _characterDatabase = characterDatabase;
+    }
+
     public void SendBlackMarketOutbidNotification(BlackMarketTemplate templ)
     {
-        BlackMarketOutbid packet = new();
-
-        packet.MarketID = templ.MarketID;
-        packet.Item = templ.Item;
-        packet.RandomPropertiesID = 0;
-
-        SendPacket(packet);
+        _session.SendPacket(new BlackMarketOutbid()
+        {
+            MarketID = templ.MarketID,
+            Item = templ.Item,
+            RandomPropertiesID = 0
+        });
     }
 
     public void SendBlackMarketWonNotification(BlackMarketEntry entry, Item item)
     {
-        BlackMarketWon packet = new();
-
-        packet.MarketID = entry.MarketId;
-        packet.Item = new ItemInstance(item);
-
-        SendPacket(packet);
+        _session.SendPacket(new BlackMarketWon()
+        {
+            MarketID = entry.MarketId,
+            Item = new ItemInstance(item)
+        });
     }
 
     [WorldPacketHandler(ClientOpcodes.BlackMarketBidOnItem)]
     private void HandleBlackMarketBidOnItem(BlackMarketBidOnItem blackMarketBidOnItem)
     {
-        if (!Global.BlackMarketMgr.IsEnabled)
+        if (!_blackMarketManager.IsEnabled)
             return;
 
-        var player = Player;
-        var unit = player.GetNPCIfCanInteractWith(blackMarketBidOnItem.Guid, NPCFlags.BlackMarket, NPCFlags2.None);
+        var unit = _session.Player.GetNPCIfCanInteractWith(blackMarketBidOnItem.Guid, NPCFlags.BlackMarket, NPCFlags2.None);
 
-        if (!unit)
+        if (unit == null)
         {
             Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} not found or you can't interact with him.", blackMarketBidOnItem.Guid.ToString());
 
             return;
         }
 
-        var entry = Global.BlackMarketMgr.GetAuctionByID(blackMarketBidOnItem.MarketID);
+        var entry = _blackMarketManager.GetAuctionByID(blackMarketBidOnItem.MarketID);
 
         if (entry == null)
         {
-            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) tried to bid on a nonexistent auction (MarketId: {2}).", player.GUID.ToString(), player.GetName(), blackMarketBidOnItem.MarketID);
+            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) tried to bid on a nonexistent auction (MarketId: {2}).", _session.Player.GUID.ToString(), _session.Player.GetName(), blackMarketBidOnItem.MarketID);
             SendBlackMarketBidOnItemResult(BlackMarketError.ItemNotFound, blackMarketBidOnItem.MarketID, blackMarketBidOnItem.Item);
 
             return;
         }
 
-        if (entry.Bidder == player.GUID.Counter)
+        if (entry.Bidder == _session.Player.GUID.Counter)
         {
-            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) tried to place a bid on an item he already bid on. (MarketId: {2}).", player.GUID.ToString(), player.GetName(), blackMarketBidOnItem.MarketID);
+            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) tried to place a bid on an item he already bid on. (MarketId: {2}).", _session.Player.GUID.ToString(), _session.Player.GetName(), blackMarketBidOnItem.MarketID);
             SendBlackMarketBidOnItemResult(BlackMarketError.AlreadyBid, blackMarketBidOnItem.MarketID, blackMarketBidOnItem.Item);
 
             return;
@@ -76,15 +85,15 @@ public class BlackMarketHandlers : IWorldSessionHandler
 
         if (!entry.ValidateBid(blackMarketBidOnItem.BidAmount))
         {
-            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) tried to place an invalid bid. Amount: {2} (MarketId: {3}).", player.GUID.ToString(), player.GetName(), blackMarketBidOnItem.BidAmount, blackMarketBidOnItem.MarketID);
+            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) tried to place an invalid bid. Amount: {2} (MarketId: {3}).", _session.Player.GUID.ToString(), _session.Player.GetName(), blackMarketBidOnItem.BidAmount, blackMarketBidOnItem.MarketID);
             SendBlackMarketBidOnItemResult(BlackMarketError.HigherBid, blackMarketBidOnItem.MarketID, blackMarketBidOnItem.Item);
 
             return;
         }
 
-        if (!player.HasEnoughMoney(blackMarketBidOnItem.BidAmount))
+        if (!_session.Player.HasEnoughMoney(blackMarketBidOnItem.BidAmount))
         {
-            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) does not have enough money to place bid. (MarketId: {2}).", player.GUID.ToString(), player.GetName(), blackMarketBidOnItem.MarketID);
+            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) does not have enough money to place bid. (MarketId: {2}).", _session.Player.GUID.ToString(), _session.Player.GetName(), blackMarketBidOnItem.MarketID);
             SendBlackMarketBidOnItemResult(BlackMarketError.NotEnoughMoney, blackMarketBidOnItem.MarketID, blackMarketBidOnItem.Item);
 
             return;
@@ -92,7 +101,7 @@ public class BlackMarketHandlers : IWorldSessionHandler
 
         if (entry.GetSecondsRemaining() <= 0)
         {
-            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) tried to bid on a completed auction. (MarketId: {2}).", player.GUID.ToString(), player.GetName(), blackMarketBidOnItem.MarketID);
+            Log.Logger.Debug("WORLD: HandleBlackMarketBidOnItem - {0} (name: {1}) tried to bid on a completed auction. (MarketId: {2}).", _session.Player.GUID.ToString(), _session.Player.GetName(), blackMarketBidOnItem.MarketID);
             SendBlackMarketBidOnItemResult(BlackMarketError.DatabaseError, blackMarketBidOnItem.MarketID, blackMarketBidOnItem.Item);
 
             return;
@@ -100,10 +109,10 @@ public class BlackMarketHandlers : IWorldSessionHandler
 
         SQLTransaction trans = new();
 
-        Global.BlackMarketMgr.SendAuctionOutbidMail(entry, trans);
-        entry.PlaceBid(blackMarketBidOnItem.BidAmount, player, trans);
+        _blackMarketManager.SendAuctionOutbidMail(entry, trans);
+        entry.PlaceBid(blackMarketBidOnItem.BidAmount, _session.Player, trans);
 
-        DB.Characters.CommitTransaction(trans);
+        _characterDatabase.CommitTransaction(trans);
 
         SendBlackMarketBidOnItemResult(BlackMarketError.Ok, blackMarketBidOnItem.MarketID, blackMarketBidOnItem.Item);
     }
@@ -111,9 +120,9 @@ public class BlackMarketHandlers : IWorldSessionHandler
     [WorldPacketHandler(ClientOpcodes.BlackMarketOpen)]
     private void HandleBlackMarketOpen(BlackMarketOpen blackMarketOpen)
     {
-        var unit = Player.GetNPCIfCanInteractWith(blackMarketOpen.Guid, NPCFlags.BlackMarket, NPCFlags2.BlackMarketView);
+        var unit = _session.Player.GetNPCIfCanInteractWith(blackMarketOpen.Guid, NPCFlags.BlackMarket, NPCFlags2.BlackMarketView);
 
-        if (!unit)
+        if (unit == null)
         {
             Log.Logger.Debug("WORLD: HandleBlackMarketHello - {0} not found or you can't interact with him.", blackMarketOpen.Guid.ToString());
 
@@ -121,21 +130,21 @@ public class BlackMarketHandlers : IWorldSessionHandler
         }
 
         // remove fake death
-        if (Player.HasUnitState(UnitState.Died))
-            Player.RemoveAurasByType(AuraType.FeignDeath);
+        if (_session.Player.HasUnitState(UnitState.Died))
+            _session.Player.RemoveAurasByType(AuraType.FeignDeath);
 
-        SendBlackMarketOpenResult(blackMarketOpen.Guid, unit);
+        SendBlackMarketOpenResult(blackMarketOpen.Guid);
     }
 
     [WorldPacketHandler(ClientOpcodes.BlackMarketRequestItems)]
     private void HandleBlackMarketRequestItems(BlackMarketRequestItems blackMarketRequestItems)
     {
-        if (!Global.BlackMarketMgr.IsEnabled)
+        if (!_blackMarketManager.IsEnabled)
             return;
 
-        var unit = Player.GetNPCIfCanInteractWith(blackMarketRequestItems.Guid, NPCFlags.BlackMarket, NPCFlags2.BlackMarketView);
+        var unit = _session.Player.GetNPCIfCanInteractWith(blackMarketRequestItems.Guid, NPCFlags.BlackMarket, NPCFlags2.BlackMarketView);
 
-        if (!unit)
+        if (unit == null)
         {
             Log.Logger.Debug("WORLD: HandleBlackMarketRequestItems - {0} not found or you can't interact with him.", blackMarketRequestItems.Guid.ToString());
 
@@ -143,27 +152,27 @@ public class BlackMarketHandlers : IWorldSessionHandler
         }
 
         BlackMarketRequestItemsResult result = new();
-        Global.BlackMarketMgr.BuildItemsResponse(result, Player);
-        SendPacket(result);
+        _blackMarketManager.BuildItemsResponse(result, _session.Player);
+        _session.SendPacket(result);
     }
 
     private void SendBlackMarketBidOnItemResult(BlackMarketError result, uint marketId, ItemInstance item)
     {
-        BlackMarketBidOnItemResult packet = new();
-
-        packet.MarketID = marketId;
-        packet.Item = item;
-        packet.Result = result;
-
-        SendPacket(packet);
+        _session.SendPacket(new BlackMarketBidOnItemResult()
+        {
+            MarketID = marketId,
+            Item = item,
+            Result = result
+        });
     }
 
-    private void SendBlackMarketOpenResult(ObjectGuid guid, Creature auctioneer)
+    private void SendBlackMarketOpenResult(ObjectGuid guid)
     {
-        NPCInteractionOpenResult npcInteraction = new();
-        npcInteraction.Npc = guid;
-        npcInteraction.InteractionType = PlayerInteractionType.BlackMarketAuctioneer;
-        npcInteraction.Success = Global.BlackMarketMgr.IsEnabled;
-        SendPacket(npcInteraction);
+        _session.SendPacket(new NPCInteractionOpenResult()
+        {
+            Npc = guid,
+            InteractionType = PlayerInteractionType.BlackMarketAuctioneer,
+            Success = _blackMarketManager.IsEnabled
+        });
     }
 }
