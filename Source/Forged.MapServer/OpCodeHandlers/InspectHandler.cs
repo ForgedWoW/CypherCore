7b8b17b7ca3a0952c8a8bb905d2d8c88092c2668
Jim Burlison <JBurlison@Gmail.com>
2023-05-11 05:54:48 -0400

    Migrate InspectHandler

--- Source/Forged.MapServer/OpCodeHandlers/InspectHandler.cs
+++ Source/Forged.MapServer/OpCodeHandlers/InspectHandler.cs
@@ -1,46 +1,65 @@
 ﻿// Copyright (c) Forged WoW LLC <https://github.com/ForgedWoW/ForgedCore>
 // Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/ForgedCore/blob/master/LICENSE> for full information.
 
-using Forged.MapServer.Entities.Players;
+using System.Linq;
+using Forged.MapServer.Globals;
+using Forged.MapServer.Guilds;
 using Forged.MapServer.Networking;
 using Forged.MapServer.Networking.Packets.Inspect;
 using Forged.MapServer.Networking.Packets.Trait;
+using Forged.MapServer.Server;
 using Framework.Constants;
+using Framework.Util;
 using Game.Common.Handlers;
+using Microsoft.Extensions.Configuration;
 using Serilog;
+
+// ReSharper disable UnusedMember.Local
 
 namespace Forged.MapServer.OpCodeHandlers;
 
 public class InspectHandler : IWorldSessionHandler
 {
+    private readonly IConfiguration _configuration;
+    private readonly GuildManager _guildManager;
+    private readonly ObjectAccessor _objectAccessor;
+    private readonly WorldSession _session;
+
+    public InspectHandler(WorldSession session, IConfiguration configuration, ObjectAccessor objectAccessor, GuildManager guildManager)
+    {
+        _session = session;
+        _configuration = configuration;
+        _objectAccessor = objectAccessor;
+        _guildManager = guildManager;
+    }
+
     [WorldPacketHandler(ClientOpcodes.Inspect, Processing = PacketProcessing.Inplace)]
     private void HandleInspect(Inspect inspect)
     {
-        var player = Global.ObjAccessor.GetPlayer(_session.Player, inspect.Target);
+        var player = _objectAccessor.GetPlayer(_session.Player, inspect.Target);
 
-        if (!player)
+        if (player == null)
         {
             Log.Logger.Debug("WorldSession.HandleInspectOpcode: Target {0} not found.", inspect.Target.ToString());
 
             return;
         }
 
-        if (!Player.Location.IsWithinDistInMap(player, SharedConst.InspectDistance, false))
+        if (!_session.Player.Location.IsWithinDistInMap(player, SharedConst.InspectDistance, false))
             return;
 
-        if (Player.WorldObjectCombat.IsValidAttackTarget(player))
+        if (_session.Player.WorldObjectCombat.IsValidAttackTarget(player))
             return;
 
         InspectResult inspectResult = new();
         inspectResult.DisplayInfo.Initialize(player);
 
-        if (Player.CanBeGameMaster || GetDefaultValue("TalentsInspecting", 1) + (Player.EffectiveTeam == player.EffectiveTeam ? 1 : 0) > 1)
+        if (_session.Player.CanBeGameMaster || _configuration.GetDefaultValue("TalentsInspecting", 1) + (_session.Player.EffectiveTeam == player.EffectiveTeam ? 1 : 0) > 1)
         {
             var talents = player.GetTalentMap(player.GetActiveTalentGroup());
 
-            foreach (var v in talents)
-                if (v.Value != PlayerSpellState.Removed)
-                    inspectResult.Talents.Add((ushort)v.Key);
+            foreach (var v in talents.Where(v => v.Value != PlayerSpellState.Removed))
+                inspectResult.Talents.Add((ushort)v.Key);
         }
 
         inspectResult.TalentTraits = new TraitInspectInfo();
@@ -54,9 +73,9 @@ public class InspectHandler : IWorldSessionHandler
 
         inspectResult.TalentTraits.Level = (int)player.Level;
 
-        var guild = Global.GuildMgr.GetGuildById(player.GuildId);
+        var guild = _guildManager.GetGuildById(player.GuildId);
 
-        if (guild)
+        if (guild != null)
         {
             InspectGuildData guildData;
             guildData.GuildGUID = guild.GetGUID();
@@ -68,13 +87,10 @@ public class InspectHandler : IWorldSessionHandler
 
         var heartOfAzeroth = player.GetItemByEntry(PlayerConst.ItemIdHeartOfAzeroth, ItemSearchLocation.Everywhere);
 
-        if (heartOfAzeroth != null)
-        {
-            var azeriteItem = heartOfAzeroth.AsAzeriteItem;
+        var azeriteItem = heartOfAzeroth?.AsAzeriteItem;
 
-            if (azeriteItem != null)
-                inspectResult.AzeriteLevel = azeriteItem.GetEffectiveLevel();
-        }
+        if (azeriteItem != null)
+            inspectResult.AzeriteLevel = azeriteItem.GetEffectiveLevel();
 
         inspectResult.ItemLevel = (int)player.GetAverageItemLevel();
         inspectResult.LifetimeMaxRank = player.ActivePlayerData.LifetimeMaxRank;
@@ -83,27 +99,27 @@ public class InspectHandler : IWorldSessionHandler
         inspectResult.LifetimeHK = player.ActivePlayerData.LifetimeHonorableKills;
         inspectResult.HonorLevel = player.PlayerData.HonorLevel;
 
-        SendPacket(inspectResult);
+        _session.SendPacket(inspectResult);
     }
 
     [WorldPacketHandler(ClientOpcodes.QueryInspectAchievements, Processing = PacketProcessing.Inplace)]
     private void HandleQueryInspectAchievements(QueryInspectAchievements inspect)
     {
-        var player = Global.ObjAccessor.GetPlayer(_session.Player, inspect.Guid);
+        var player = _objectAccessor.GetPlayer(_session.Player, inspect.Guid);
 
-        if (!player)
+        if (player == null)
         {
-            Log.Logger.Debug("WorldSession.HandleQueryInspectAchievements: [{0}] inspected unknown Player [{1}]", Player.GUID.ToString(), inspect.Guid.ToString());
+            Log.Logger.Debug("WorldSession.HandleQueryInspectAchievements: [{0}] inspected unknown Player [{1}]", _session.Player.GUID.ToString(), inspect.Guid.ToString());
 
             return;
         }
 
-        if (!Player.Location.IsWithinDistInMap(player, SharedConst.InspectDistance, false))
+        if (!_session.Player.Location.IsWithinDistInMap(player, SharedConst.InspectDistance, false))
             return;
 
-        if (Player.WorldObjectCombat.IsValidAttackTarget(player))
+        if (_session.Player.WorldObjectCombat.IsValidAttackTarget(player))
             return;
 
-        player.SendRespondInspectAchievements(Player);
+        player.SendRespondInspectAchievements(_session.Player);
     }
 }
