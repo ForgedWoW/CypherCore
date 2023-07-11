e24e474c3bf83f188938cd06222f75d69f689c95
Jim Burlison <JBurlison@Gmail.com>
2023-07-11 16:28:19 -0400

    Fix BG Script

--- Source/Forged.MapServer/Scripting/BaseScripts/BattlegroundMapScript.cs
+++ Source/Forged.MapServer/Scripting/BaseScripts/BattlegroundMapScript.cs
@@ -2,18 +2,17 @@
 // Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/ForgedCore/blob/master/LICENSE> for full information.
 
 using Forged.MapServer.Maps;
+using Forged.MapServer.Scripting.Interfaces;
 using Serilog;
 
 namespace Forged.MapServer.Scripting.BaseScripts;
 
-public class BattlegroundMapScript : MapScript<BattlegroundMap>
+public class BattlegroundMapScript : MapScript<BattlegroundMap>, IScriptAutoAdd
 {
     public BattlegroundMapScript(string name, uint mapId) : base(name, mapId)
     {
         if (GetEntry() != null &&
             GetEntry().IsBattleground())
             Log.Logger.Error("BattlegroundMapScript for map {0} is invalid.", mapId);
-
-        ScriptManager.AddScript(this);
     }
 }
