18c2aa6b1ce0375019d53a790e369b9e0584609a
Jim Burlison <JBurlison@Gmail.com>
2023-07-11 16:38:26 -0400

    Fix Map script

--- Source/Forged.MapServer/Scripting/BaseScripts/MapScript.cs
+++ Source/Forged.MapServer/Scripting/BaseScripts/MapScript.cs
@@ -2,7 +2,7 @@
 // Licensed under GPL-3.0 license. See <https://github.com/ForgedWoW/ForgedCore/blob/master/LICENSE> for full information.
 
 using System.Collections.Generic;
-using Forged.MapServer.DataStorage;
+using Forged.MapServer.DataStorage.ClientReader;
 using Forged.MapServer.DataStorage.Structs.M;
 using Forged.MapServer.Maps;
 using Serilog;
@@ -11,19 +11,27 @@ namespace Forged.MapServer.Scripting.BaseScripts;
 
 public class MapScript<T> : ScriptObject where T : Map
 {
-    private readonly MapRecord _mapEntry;
+    private readonly uint _mapId;
+    private MapRecord _mapEntry;
+    private DB6Storage<MapRecord> _mapRecords;
 
     public MapScript(string name, uint mapId) : base(name)
     {
-        _mapEntry = CliDB.MapStorage.LookupByKey(mapId);
-
-        if (_mapEntry == null)
-            Log.Logger.Error("Invalid MapScript for {0}; no such map ID.", mapId);
+        _mapId = mapId;
     }
 
     // Gets the MapEntry structure associated with this script. Can return NULL.
     public MapRecord GetEntry()
     {
+        if (_mapEntry != null || _mapRecords != null)
+            return _mapEntry;
+
+        _mapRecords = ClassFactory.Resolve<DB6Storage<MapRecord>>();
+        _mapEntry = _mapRecords.LookupByKey(_mapId);
+
+        if (_mapEntry == null)
+            Log.Logger.Error("Invalid MapScript for {0}; no such map ID.", _mapId);
+
         return _mapEntry;
     }
 }
